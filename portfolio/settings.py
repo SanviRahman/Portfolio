057fd2cdfe7056e9062cb05925787881e7c4e511
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-03-27 15:57:30 +0600

    mahid

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,17 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# MEDIA_URL='/images/'
+STATIC_URL = '/static/'
+MEDIA_URL='/images/'
 
-# STATICFILES_DIRS=[
-#   os.path.join(BASE_DIR, 'static')  
-# ]
+STATICFILES_DIRS=[
+  os.path.join(BASE_DIR, 'static')  
+]
 
-STATIC_URL = 'static/'
-STATICFILES_DIR= os.path.join(BASE_DIR, 'static')
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build','static')
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
